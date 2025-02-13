411001de21f57ddf9b0c76e536cce9af2f19b697
diyagaur10 <146921988+diyagaur10@users.noreply.github.com>
2025-02-14 00:51:29 +0530

    Time: 0 ms (100%), Space: 9.2 MB (52.42%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        int xorr = 0;
+        for(char c : s) xorr ^= c;
+        for(char c : t) xorr ^= c;
+        return xorr;
+    }
+};
