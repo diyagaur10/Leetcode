66c37c526dd10128914dcfc74787301e8a42a213
diyagaur10 <146921988+diyagaur10@users.noreply.github.com>
2025-06-17 22:45:16 +0530

    Time: 1 ms (69.13%), Space: 15.4 MB (12.55%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for (int num : nums) {
-            freq[num]++;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
         }
 
-        sort(nums.begin(), nums.end(), [&](int a, int b) {
-            if (freq[a] == freq[b]) return a > b; // decreasing value if freq same
-            return freq[a] < freq[b];             // increasing frequency
-        });
+        // Custom comparator inside lambda
+        auto cmp = [](pair<int, int>& a, pair<int, int>& b) {
+            if (a.first == b.first) return a.second < b.second; // higher value first
+            return a.first > b.first; // lower frequency first
+        };
 
-        return nums;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> minh(cmp);
+
+        for (auto& it : mp) {
+            minh.push({it.second, it.first});
+        }
+
+        vector<int> ans;
+        while (!minh.empty()) {
+            int freq = minh.top().first;
+            int val = minh.top().second;
+            for (int i = 0; i < freq; i++) {
+                ans.push_back(val);
+            }
+            minh.pop();
+        }
+
+        return ans;
     }
 };
