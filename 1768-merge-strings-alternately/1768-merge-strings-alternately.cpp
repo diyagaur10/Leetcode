9573d8dba4716f7db653a3845eff282205e06811
diyagaur10 <146921988+diyagaur10@users.noreply.github.com>
2025-08-25 17:43:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (63.26%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int p1 = 0, p2 = 0;
+        string ans = "";
+        while(p1 < word1.size() && p2 < word2.size()){
+            ans.push_back(word1[p1]);
+            p1++;
+            ans.push_back(word2[p2]);
+            p2++;
+        }
+        while (p1 < word1.size()) {
+            ans.push_back(word1[p1++]);
+        }
+
+        // Agar word2 lamba hai
+        while (p2 < word2.size()) {
+            ans.push_back(word2[p2++]);
+        }
+            return ans;
+        
+    }
+};
