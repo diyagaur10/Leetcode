c89d5298784791610fe9b296efde8eb505113919
DIYA GAUR <146921988+diyagaur10@users.noreply.github.com>
2025-10-30 19:22:39 +0530

    Time: 2 ms (20.26%), Space: 11.9 MB (78.16%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> freq;
+        for (int num : arr)
+            freq[num]++;  
+
+        unordered_set<int> seen;
+        for (auto& [num, count] : freq) {
+            if (seen.count(count)) return false; // Duplicate occurrence count found
+            seen.insert(count);
+        }
+        return true;
+    }
+};
