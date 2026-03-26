b1be1df1105088fd32632e8c3bb6948b1b242abe
DIYA GAUR <146921988+diyagaur10@users.noreply.github.com>
2026-03-26 23:35:16 +0530

    Time: 3 ms (6.15%), Space: 10.3 MB (5.23%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countCommasPerNumber(long long num) {
+        int len = std::to_string(num).length();
+        if (len % 3 == 0) {
+            return (len / 3) - 1;
+        } else {
+            return len / 3;
+        }
+    }
+
+    long long countCommas(long long n) {
+        long long total = 0;
+
+        long long start = 1;
+
+        for (int len = 1; len <= 18; len++) {
+            long long end = start * 10 - 1;
+
+            if (start > n) break;
+
+            end = min(n, end);
+
+            long long count = end - start + 1;
+
+            int commas = countCommasPerNumber(start);
+
+            total += count * commas;
+
+            start *= 10;
+        }
+
+        return total;
+    }
+};
